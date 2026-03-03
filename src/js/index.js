860979eccd3f63ca28cc98ed38f1a1d05b9110fe
Charismatic30 <132722351+Charismatic30@users.noreply.github.com>
2026-03-03 23:08:03 +0900

    button functionality added

--- src/js/index.js
+++ src/js/index.js
@@ -18,45 +18,42 @@ import '../scss/style.scss'
         
       });
 
+//   // if (typeof swiper !== 'undefined') {
+//   //   swiper.update(); 
+//   // }
+// }
+
+
 const showBtn = document.getElementById('showButton');
 const hideBtn = document.getElementById('hideButton');
-const bottomItems = document.querySelectorAll('.swiper');
+const hideBottom = document.querySelectorAll('.brands-repairs .swiper');
 
 function hideBottomGrid(show) {
-  bottomItems.forEach(item => {
+  hideBottom.forEach(item => {
     item.style.height = show ? 'auto' : '200px'; 
     item.style.overflow = show ? 'visible' : 'hidden';
   });
 
-
   showBtn.style.display = show ? 'none' : 'flex';
   hideBtn.style.display = show ? 'flex' : 'none';
-
-  // if (typeof swiper !== 'undefined') {
-  //   swiper.update(); 
-  // }
 }
 
 showBtn.addEventListener('click', () => hideBottomGrid(true));
 hideBtn.addEventListener('click', () => hideBottomGrid(false));
 
-
-
 const showSecondBtn = document.getElementById('showSecondButton');
 const hideSecondBtn = document.getElementById('hideSecondButton');
-const secondBottomItems = document.querySelectorAll('.bottom-hide');
+const hideSecondBottom = document.querySelectorAll('.brand-repairs .swiper');
 
 function hideSecondBottomGrid(show) {
-secondBottomItems.forEach(item => {
-  item.style.height = show ? 'auto' : '200px';
-  item.style.overflow = show ? 'visible' : 'hidden';
-});
+  hideSecondBottom.forEach(item => {
+    item.style.height = show ? 'auto' : '200px'; 
+    item.style.overflow = show ? 'visible' : 'hidden';
+  });
 
- showSecondBtn.style.display = show ? 'none' : 'flex';
+  showSecondBtn.style.display = show ? 'none' : 'flex';
   hideSecondBtn.style.display = show ? 'flex' : 'none';
-
 }
-
 
 showSecondBtn.addEventListener('click', () => hideSecondBottomGrid(true));
 hideSecondBtn.addEventListener('click', () => hideSecondBottomGrid(false));
