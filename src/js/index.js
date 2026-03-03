ed35caedd664bdecbd15eadf7adb6fa1a2a400e1
Charismatic30 <132722351+Charismatic30@users.noreply.github.com>
2026-03-03 20:54:08 +0900

    try again

--- src/js/index.js
+++ src/js/index.js
@@ -32,12 +32,32 @@ function hideBottomGrid(show) {
   showBtn.style.display = show ? 'none' : 'flex';
   hideBtn.style.display = show ? 'flex' : 'none';
 
- 
   // if (typeof swiper !== 'undefined') {
   //   swiper.update(); 
   // }
 }
 
-
 showBtn.addEventListener('click', () => hideBottomGrid(true));
 hideBtn.addEventListener('click', () => hideBottomGrid(false));
+
+
+
+const showSecondBtn = document.getElementById('showSecondButton');
+const hideSecondBtn = document.getElementById('hideSecondButton');
+const secondBottomItems = document.querySelectorAll('.bottom-hide');
+
+function hideSecondBottomGrid(show) {
+secondBottomItems.forEach(item => {
+  item.style.height = show ? 'auto' : '200px';
+  item.style.overflow = show ? 'visible' : 'hidden';
+});
+
+ showSecondBtn.style.display = show ? 'none' : 'flex';
+  hideSecondBtn.style.display = show ? 'flex' : 'none';
+
+}
+
+
+showSecondBtn.addEventListener('click', () => hideSecondBottomGrid(true));
+hideSecondBtn.addEventListener('click', () => hideSecondBottomGrid(false));
+
